c37cba30c8a1896cc762567050824f590745cffb
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2022-01-13 22:22:22 +0100

    WIP

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,19 @@
 class MessagesController < ApplicationController
-
   def index
     @messages = Message.all
     @message = Message.new
   end
 
   def create
-    @message = Message.create(content: params["message"]["content"])
+    @message = Message.create(content: params['message']['content'])
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append(:message, partial: 'messages/message', locals: { message: @message })
+      end
+
+      format.html { redirect_to messages_url }
+    end
   end
 
   def destroy
@@ -15,9 +22,8 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream { render turbo_stream: turbo_stream.remove(@message) }
-      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
 
   after_create_commit { broadcast_append_to "messages" }
 
+  after_destroy_commit -> { broadcast_remove_to self }
 end
